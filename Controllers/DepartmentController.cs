f3f7ca1e18dd5e0493a88fc68bed380f08f7c829
Manuel De La Torre <dmanuel990@gmail.com>
2025-02-28 08:33:13 -0500

    feat: se agrego DepartmentController y DepartmentServices para la obtencion de los municipios y departamentos

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,56 @@
+﻿using BackOlSoftware.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackOlSoftware.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly DepartmentService _departmentService;
+
+        public DepartmentController(DepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+
+        [HttpGet("Departments")]
+        public async Task<IActionResult> getDepartments()
+        {
+            try
+            {
+                var departments = await _departmentService.getDepartments();
+
+                if (departments == null || departments.Count == 0)
+                    return NotFound(new { status = 404, msg = "No se encontraron municipios" });
+
+                return Ok(new { status = 200, msg = "Lista de municipios obtenida con éxito", data = departments });
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, new { status = 500, msg = "Error interno", error = e.Message });
+            }
+        }
+
+        [HttpGet("municipalities/{department}")]
+        public async Task<IActionResult> GetMunicipalitiesByDepartment(string department)
+        {
+            try
+            {
+                var municipalities = await _departmentService.GetMunicipalitiesByDepartmentAsync(department);
+                if (municipalities == null || !municipalities.Any())
+                    return NotFound(new { status = 404, msg = $"No se encontraron municipios en {department}" });
+
+                return Ok(new { status = 200, msg = $"Lista de municipios de {department} obtenida con éxito", data = municipalities });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { status = 500, msg = "Error interno", error = ex.Message });
+            }
+        }
+
+    }
+}
---
+++ Services/DepartmentService.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Text.Json;
+using BackOlSoftware.Models;
+
+namespace BackOlSoftware.Services
+{
+    public class DepartmentService
+    {
+        private readonly HttpClient _httpClient;
+        private const string ApiUrl = "https://www.datos.gov.co/resource/xdk5-pm3f.json";
+
+
+        public DepartmentService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<string>> getDepartments()
+        {
+            try
+            {
+                var response = await _httpClient.GetStreamAsync(ApiUrl);
+
+                var locations = JsonSerializer.Deserialize<List<Department>>(response) ?? new List<Department>();
+
+                var departments = locations
+                    .Where(l => !string.IsNullOrEmpty(l.Departamento))
+                    .Select(l => l.Departamento)
+                    .Distinct()
+                    .OrderBy(d => d)
+                    .ToList();
+
+                return departments;
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Error al obtener los municipios", e);
+            }
+        }
+
+        public async Task<List<string>> GetMunicipalitiesByDepartmentAsync(string department)
+        {
+            var cacheKey = $"municipalities_{department}";
+
+            
+            try
+            {
+                var url = $"{ApiUrl}?departamento={Uri.EscapeDataString(department)}";
+                var response = await _httpClient.GetStringAsync(url);
+                var locations = JsonSerializer.Deserialize<List<Department>>(response) ?? new List<Department>();
+
+                var municipalities = locations
+                    .Where(l => !string.IsNullOrEmpty(l.Municipio))
+                    .Select(l => l.Municipio)
+                    .Distinct()
+                    .OrderBy(m => m)
+                    .ToList();
+
+                return municipalities;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error al obtener municipios de {department}", ex);
+            }
+        }
+
+    }
+}
